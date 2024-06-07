8f1e5294148a444de8e0c1f9d01c030c9011da42
Robert Zhang <louirobert@gmail.com>
2024-06-07 13:11:17 +0800

    Modify function/nameref.sh, demonstrating array reference.

--- function/nameref.sh
+++ function/nameref.sh
@@ -4,13 +4,16 @@
 # https://www.gnu.org/software/bash/manual/html_node/Shell-Parameters.html
 
 declare -i x=100
+declare -a a=('a' 'b' 'c')
 
 function f {
   local -n ref_x=$1
-  echo "f before: ref_x=$ref_x"
+  local -n ref_a=$2
+  echo "f before: ref_x=$ref_x ref_a=${a[@]}"
   ref_x+=10
-  echo "f after: ref_x=$ref_x"
+  ref_a+=('d' 'e')
+  echo "f after: ref_x=$ref_x ref_a=${a[@]}"
 }
 
-f x
-echo "x=$x"
+f x a
+echo "x=$x ref_a=${a[@]}"

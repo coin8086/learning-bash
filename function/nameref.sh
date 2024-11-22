a80a378efd512a0eec384950a23a6dea4d699eaf
Robert Zhang <louirobert@gmail.com>
2024-11-22 17:50:52 +0800

    Modify nameref.sh

--- function/nameref.sh
+++ function/nameref.sh
@@ -10,15 +10,27 @@ function f1 {
   ref+=1
   echo "f1: ref=$ref"
 
+  local y="hello"
+
   # NOTE: Here the name $1 is passed to f2.
   # Also note that the variable of name $1, is visible to f2.
-  f2 $1
+  f2 $1 ref y z
 }
 
 function f2 {
   local -n ref=$1
   ref+=1
   echo "f2: ref=$ref"
+
+  local -n ref2=$2
+  ref2+=1
+  echo "f2: ref2=$ref2"
+
+  local -n ref3=$3
+  echo "f2: ref3=$ref3"
+
+  local -n ref4=$4
+  echo "f2: ref4=$ref4"
 }
 
 f1 x

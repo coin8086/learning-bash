7769b4476ed18ced5726d314afc66aeb38a909bd
Robert Zhang <louirobert@gmail.com>
2025-05-21 10:51:55 +0800

    Add comments in function/scope.sh, explaining Bash's dynamic scoping.

--- function/scope.sh
+++ function/scope.sh
@@ -10,6 +10,13 @@ function f1 {
   x='Hello from f1'
 }
 
+# "The shell uses dynamic scoping to control a variable’s visibility within functions.
+# With dynamic scoping, visible variables and their values are a result of the sequence
+# of function calls that caused execution to reach the current function. The value of a
+# variable that a function sees depends on its value within its caller, if any, whether
+# that caller is the "global" scope or another shell function. This is also the value
+# that a local variable declaration "shadows", and the value that is restored when the
+# function returns."
 function f2 {
   local x
   echo "f2: x=$x"
